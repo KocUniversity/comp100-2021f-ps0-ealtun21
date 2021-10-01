9110e1579812b27af5e7a4a0cbcd3e78726cb582
Eray Altun <ealtun21@ku.edu.tr>
2021-10-01 18:04:27 +0300

    personal error checking version

---
+++ main-personal.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import numpy
+
+try:                                                                                #Error handling  
+    x = input("Please enter the value of x:")                                       #Ask for x value
+    y = input("Please enter the value of y:")                                       #Ask for y value
+    print("The value of x raised to the power of y is:",float(x)**float(y))
+    print("The log of x is:",numpy.log2(float(x)))
+    print("My student id is: 0077808")
+except ValueError as e:
+    print("Please only input numbers")          #Crash message
+except KeyboardInterrupt as e:
+    print("\n(×_×)")
+except:
+    print("Ouch! you broke the universe!")
