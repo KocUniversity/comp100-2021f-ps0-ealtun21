80783114947cf5f48e73b8a66939becd04ad4c71
Eray Altun <ealtun21@ku.edu.tr>
2021-10-01 17:16:41 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print ("Hello World!")
+#!/usr/bin/env python3
+import numpy
+
+x = input("Please enter the value of x:") #Ask for x value
+y = input("Please enter the value of y:") #Ask for y value
+print("\n")
+print("The value of x raised to the power of y is:",float(x)**float(y))
+print("The log of x is:",numpy.log2(float(x)))
+print("My student id is: 0077808")
