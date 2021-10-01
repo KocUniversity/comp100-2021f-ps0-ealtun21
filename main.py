55a31486ef066795d8fed8aea6d74e2615c554d5
Eray Altun <ealtun21@ku.edu.tr>
2021-10-01 18:02:15 +0300

    changed aesthetics

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import numpy
 
 x = input("Please enter the value of x:") #Ask for x value
 y = input("Please enter the value of y:") #Ask for y value
-print("\n")
 print("The value of x raised to the power of y is:",float(x)**float(y))
 print("The log of x is:",numpy.log2(float(x)))
 print("My student id is: 0077808")
